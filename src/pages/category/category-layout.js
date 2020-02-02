8fd60279903e491773b400d9ab22117f0835c627
Eduardo Pinedo <eduyy_24@hotmail.com>
2020-02-02 14:48:10 -0500

    components created, and button end cart implemented

--- src/pages/category/category-layout.js
+++ src/pages/category/category-layout.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import EndButtonCart from './components/end-button-cart';
 
 export default function CategoryLayout(props) {
   return (
-    <View>
-      <Text>layout</Text>
+    <View style={styles.container}>
+      <View style={styles.section1}>
+        <Text>1</Text>
+      </View>
+      <View style={styles.section2}>
+        <View style={styles.sectionRoderBy}>
+          <Text>2</Text>
+        </View>
+        <EndButtonCart />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  section1: {
+    flex: 0.7,
+    backgroundColor: 'white',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    marginHorizontal: 16,
+    elevation: 5,
+  },
+  section2: {
+    flex: 0.3,
+  },
+  sectionRoderBy: {
+    flex: 1,
+  },
+});
---
+++ src/pages/category/components/end-button-cart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {TouchableOpacity, Text, StyleSheet, View} from 'react-native';
+import {secundaryColor, general} from '../../../styles/styles';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default function EndButtonCart(params) {
+  return (
+    <TouchableOpacity style={styles.containerbutton}>
+      <View style={general.horizontal}>
+        <Icon name="cart-arrow-down" size={24} color="white" />
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={general.horizontal}>
+        <Text style={styles.text}>IR AL CARRITO</Text>
+        <Icon name="chevron-right" size={24} color="white" />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerbutton: {
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: secundaryColor,
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+  },
+  text: {
+    color: 'white',
+    ...general.medium14,
+  },
+});
