ab0591e921f72d841d3a0ff6fe1266810b5e06a8
Eduardo Pinedo <eduyy_24@hotmail.com>
2020-02-03 12:53:19 -0500

    more documents

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -5,6 +5,11 @@ import * as actionsMap from '../../actions/actions';
 import CategoryLayout from './category-layout';
 import ModalFilter from './components/modal-filter';
 
+/**
+ * Pagina encargada de la visualización de la categoría seleccionada, y gestión de la mismas,
+ * como poder visualizar la sub-categorías de esta, ver los productos asociados a la categoría y sub-categorías,
+ * asi como tambien, ordenar y filtrar los productos.
+ */
 class Category extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +18,6 @@ class Category extends Component {
   state = {
     subCategories: [],
     productsShow: [],
-    auxProductsShow: [],
     visibleModal: false,
   };
 
@@ -24,20 +28,29 @@ class Category extends Component {
     //se extrae el id de la categoría para consultar toda la información de la misma.
     const id = navigation.getParam('id');
     subCategories.push(categories[id].sublevels);
+
+    // filtrado inicial de los productos de la categoría
     const productsFiltered = products.filter(
       product => product.sublevel_id === id,
     );
     this.setState({
       subCategories,
       productsShow: productsFiltered,
-      auxProductsShow: productsFiltered,
     });
   }
 
+  /**
+   * Genera secciones de sub-categorías una debajo de la otra en función de las sub-categorias asociadas
+   * a las mismas sub-categorías, este comportamiento es dianamico y puede generar cuantos niveles tengan asociados
+   * las sub-categorias.
+   * @param {number} id id de la subcategoria
+   * @param {number} indexSection identificador de la sección desde la cual se generó el evento
+   * @param {Array} data Array con los objetos de las sub-categorias a insertar o remplazar en la nueva sección
+   */
   onPressShipSubCategory = (id, indexSection, data) => {
     const {subCategories} = this.state;
     const {products} = this.props;
-
+    // Se inserta la sección de sub-categoría o se reemplaza si ya existe
     if (data) {
       if (subCategories[indexSection]) {
         subCategories[indexSection] = data;
@@ -49,39 +62,45 @@ class Category extends Component {
         subCategories.splice(indexSection, 1);
       }
     }
+    // se eliminan la secciones de subcategorias que no pertenecen a la opción seleccionada
     if (subCategories[indexSection + 1]) {
       for (var i = indexSection + 1; i < subCategories.length; i++) {
         subCategories.splice(i, 1);
       }
     }
 
+    // Se filtran de la lista de productos los asociados a la sub-categoría seleccionada
     const productsFiltered = products.filter(
       product => product.sublevel_id === id,
     );
     this.setState({
       subCategories,
       productsShow: productsFiltered,
-      auxProductsShow: productsFiltered,
     });
   };
 
+  /**
+   * Función que ordena los productos tomando como parametro el tipo de boton presionado,
+   * dichos metodos de ordenamiento son de manera ascendente
+   * @param {string} type Tipo de ordenamiento.
+   */
   onPressOrder = type => {
-    var {productsShow, auxProductsShow} = this.state;
+    var {productsShow} = this.state;
     switch (type) {
       case 'price':
-        productsShow = auxProductsShow.sort((a, b) => {
+        productsShow = productsShow.sort((a, b) => {
           const value1 = a.price.replace('$', '').replace(',', '');
           const value2 = b.price.replace('$', '').replace(',', '');
           return value1 - value2;
         });
         break;
       case 'available':
-        productsShow = auxProductsShow.sort((a, b) => {
+        productsShow = productsShow.sort((a, b) => {
           return b.available - a.available;
         });
         break;
       case 'quantity':
-        productsShow = auxProductsShow.sort((a, b) => {
+        productsShow = productsShow.sort((a, b) => {
           return a.quantity - b.quantity;
         });
         break;
@@ -89,14 +108,20 @@ class Category extends Component {
     this.setState({productsShow});
   };
 
+  /** Muestra el modal con las opciones de filtrado */
   onPressfilter = () => {
     this.setState({visibleModal: true});
   };
 
+  /** Oculta el modal con las opciones de filtrado */
   onPressCloseModal = () => {
     this.setState({visibleModal: false});
   };
 
+  /**
+   * Agrega el producto al array de productos del carrito de compras, y los actualiza en el store
+   * @param {Object} product producto a agregar al carrito de compras
+   */
   onPressAddCart = product => {
     const {actions, cart} = this.props;
     //Se agrega el atributo multiplier para la gestión de la cantidad de productos en la page de Cart
@@ -105,18 +130,14 @@ class Category extends Component {
     this.setState({productsShow: this.state.productsShow}); //obligar a hacer re-render
   };
 
+  /**Función que actualiza la lista de productos a mostrar despues de ser filtrados */
   onPressFilterModal = products => {
-    this.setState({productsShow: products, auxProductsShow: products});
+    this.setState({productsShow: products});
     this.onPressCloseModal();
   };
 
   render() {
-    const {
-      subCategories,
-      productsShow,
-      visibleModal,
-      auxProductsShow,
-    } = this.state;
+    const {subCategories, productsShow, visibleModal} = this.state;
     const {cart, navigation} = this.props;
     return (
       <CategoryLayout
@@ -132,7 +153,7 @@ class Category extends Component {
           <ModalFilter
             visibleModal={visibleModal}
             onPressCloseModal={this.onPressCloseModal}
-            products={auxProductsShow}
+            products={productsShow}
             onPressFilterModal={this.onPressFilterModal}
           />
         }
@@ -141,6 +162,7 @@ class Category extends Component {
   }
 }
 
+// mapea el estado global a la propiedades locales de la pagina
 function mapStateToProps(state) {
   return {
     categories: state.productsReducer.categories,
@@ -149,6 +171,7 @@ function mapStateToProps(state) {
   };
 }
 
+// mapea las acciones disponibles sobre el store para su consumo
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(actionsMap, dispatch),
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,13 +6,28 @@ import * as actionsMap from '../../actions/actions';
 import categoriesFile from '../../resources/categories.json';
 import productsFile from '../../resources/products.json';
 
+/**
+ * Pagina encargada de renderizar la primera vista de la aplicación, permitiendo la visualización
+ * de las categorias principales dentro del inventario de productos del cliente, dichas categorias y productos se
+ * consumen de un archivo .JSON suministrado. Esta informacíon es persistida en el store de redux,
+ * para su respectiva manipulación
+ */
+
 class Home extends Component {
-  async componentDidMount() {
+  /**
+   * Al iniciar la aplicación se carga la información de los archivos al store de redux, usando las acciones
+   * definidas para esto.
+   */
+  componentDidMount() {
     const {actions} = this.props;
     actions.setCategoriesStore(categoriesFile.categories);
     actions.setProductStore(productsFile.products);
   }
 
+  /**
+   * Función que redirecciona a la página de Category para manipular la información de la misma,
+   * pasando el "id" de la categoria como parametro.
+   */
   onPressCategory = id => {
     const {navigation} = this.props;
     navigation.navigate('Category', {id});
@@ -29,12 +44,14 @@ class Home extends Component {
   }
 }
 
+// mapea el estado global a la propiedades locales de la pagina
 function mapStateToProps(state) {
   return {
     categories: state.productsReducer.categories,
   };
 }
 
+// mapea las acciones disponibles sobre el store para su consumo
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(actionsMap, dispatch),
